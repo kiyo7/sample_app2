e3610d480a56ce92f56790d610b2dbc0d5361192
kiyo <kiyo324783@icloud.com>
2021-03-28 07:27:52 +0000

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i #正規表現を定数で定義する　大文字の変数はRubyでは定数になる
   validates :email, presence: true, length: { maximum: 255 },
@@ -31,13 +31,36 @@ class User < ApplicationRecord
     end
    
     # 渡されたトークンがダイジェストと一致したらtrueを返す
-    def authenticated?(remember_token) #remember_tokenは二行目のattr_accessorのものとは別物
-      return false if remember_digest.nil? #記憶ダイジェストがnilの場合、falseを戻り値として返す
-      BCrypt::Password.new(remember_digest).is_password?(remember_token) #bcryptを使ってcookies[:remember_token]がremember_digestと一致することを確認
+    def authenticated?(attribute, token)
+      digest = send("#{attribute}_digest")
+      return false if digest.nil?
+      BCrypt::Password.new(digest).is_password?(token)
     end
-    
      # ユーザーのログイン情報を破棄する
     def forget
       update_attribute(:remember_digest, nil) #remember_digestをnilに変更
+    end
+    
+    # アカウントを有効にする
+    def activate
+      update_columns(activated: true, activated_at: Time.zone.now)
+    end
+    
+    # 有効化用のメールを送信する
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
+    
+    private
+    
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      email.downcase!
+    end
+    
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
